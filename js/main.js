03b6fa990a8599a223eed4d680816d319bc9e594
Kayla Jean Crane <kayla.crane@gmail.com>
2020-06-30 09:22:46 +0200

    add else if statements for over 100, too high, too low

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 'use strict';
 
-//SELECTORS document.querySelector("");
+//SELECTORS -- document.querySelector("");
 const secretNumber = getRandomNumber(100);
 console.log(`El número aleatorio es ${secretNumber}`);
+
+const userInput = document.querySelector('.js-user-number');
+let userNumber = 0;
+
 const cluesMsg = document.querySelector('.js-clues-msg');
 const attemptsMsg = document.querySelector('.js-attempts-msg');
+
 const button = document.querySelector('.js-submit-button');
-let userInput = document.querySelector('.js-user-number');
-let userNumber = 0;
+
 let attempts = 0;
+
+/*set default cluesMsg text*/
+cluesMsg.innerHTML = 'Escribe un número y dale a Prueba';
+cluesMsg.style.color = 'blue';
 
 //FUNCTIONS
 function getRandomNumber(max) {
@@ -21,12 +29,25 @@ function getUserNumber() {
 }
 function compareNumbers() {
   attempts += 1;
-  console.log(attempts);
-  console.log(typeof userNumber, typeof secretNumber);
+  attemptsMsg.innerHTML = attempts;
+  console.log('Número de intentos', attempts);
   if (userNumber === secretNumber) {
     console.log(userNumber, secretNumber);
+    /*display success msg*/
     cluesMsg.innerHTML = 'Has ganado campeona!!!';
-    cluesMsg.style.color = 'green';
+    cluesMsg.style.color = 'yellowgreen';
+  } else if (userNumber > 100) {
+    /*display over 100 msg*/
+    cluesMsg.innerHTML = 'El número debe estar entre 1 y 100';
+    cluesMsg.style.color = 'darkorange';
+  } else if (userNumber > secretNumber) {
+    /*display too high msg*/
+    cluesMsg.innerHTML = 'Demasiado alto...';
+    cluesMsg.style.color = 'red';
+  } else if (userNumber < secretNumber) {
+    /*display too low msg*/
+    cluesMsg.innerHTML = 'Demasiado bajo...';
+    cluesMsg.style.color = 'red';
   }
 }
 function buttonEventHandler() {
