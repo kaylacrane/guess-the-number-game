8cc8409084caa622fa699b440cff1e5650540a7b
Kayla Jean Crane <kayla.crane@gmail.com>
2020-06-30 08:49:12 +0200

    add variables and function for getting user input

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 'use strict';
 
-//SELECTORS
+//SELECTORS document.querySelector("");
+const secretNumber = getRandomNumber(100);
+console.log(`El número aleatorio es ${secretNumber}`);
+const button = document.querySelector('.js-submit-button');
+let userInput = document.querySelector('.js-user-number');
+let userNumber = 0;
+let attempts = 0;
 
 //FUNCTIONS
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+function getUserNumber() {
+  userNumber = userInput.value;
+  console.log(`La usuaria ha elegido ${userNumber}`);
+}
+function compareNumbers() {}
+function buttonEventHandler() {
+  getUserNumber();
+  compareNumbers();
+}
 
 //LISTENERS
+button.addEventListener('click', getUserNumber);
