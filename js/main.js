664f8241861bfb8979970160c7916600f969bc85
Kayla Jean Crane <kayla.crane@gmail.com>
2020-06-30 09:01:30 +0200

    add function if statement to compare secretNumber to userNumber when successful

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 //SELECTORS document.querySelector("");
 const secretNumber = getRandomNumber(100);
 console.log(`El número aleatorio es ${secretNumber}`);
+const cluesMsg = document.querySelector('.js-clues-msg');
+const attemptsMsg = document.querySelector('.js-attempts-msg');
 const button = document.querySelector('.js-submit-button');
 let userInput = document.querySelector('.js-user-number');
 let userNumber = 0;
@@ -13,14 +15,24 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 function getUserNumber() {
-  userNumber = userInput.value;
+  userNumber = parseInt(userInput.value);
   console.log(`La usuaria ha elegido ${userNumber}`);
+  return userNumber;
 }
-function compareNumbers() {}
+function compareNumbers() {
+  attempts += 1;
+  console.log(attempts);
+  console.log(typeof userNumber, typeof secretNumber);
+  if (userNumber === secretNumber) {
+    console.log(userNumber, secretNumber);
+    cluesMsg.innerHTML = 'Has ganado campeona!!!';
+    cluesMsg.style.color = 'green';
+  }
+}
 function buttonEventHandler() {
   getUserNumber();
   compareNumbers();
 }
 
 //LISTENERS
-button.addEventListener('click', getUserNumber);
+button.addEventListener('click', buttonEventHandler);
